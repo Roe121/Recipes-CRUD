dbff5f7b00dc9c330ee0b34a55000aef6b7aceb4
Roe Lez <lezzarrami@gmail.com>
2024-05-05 00:54:47 +0200

    Ingredient fixtures / ingredients show list / list pagination

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\IngredientRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class IngredientController extends AbstractController
+{
+    #[Route('/ingredient', name: 'app_ingredient')]
+    public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
+    {
+
+        $ingredients = $paginator->paginate(
+            $repository->findAll(), 
+            $request->query->getInt('page', 1), 
+            7
+        );
+
+
+        return $this->render('pages/ingredient/index.html.twig', [
+            'ingredients' => $ingredients
+        ]);
+    }
+}
